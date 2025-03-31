bcddfccc9017bb7aa7ced865c47c5126997286b8
Daisy-GonzalezLita12 <22620056@tlaxiaco.tecnm.mx>
2025-03-30 21:52:32 -0600

    Agregación de saltos

--- src/components/Props/Padre2.vue
+++ src/components/Props/Padre2.vue
@@ -10,10 +10,10 @@ const count = ref(0)
 <template>
     <div class="container">
         <!-- Título -->
-        <h1 style="color: darkgreen;">Hola soy el componente padre</h1>
+        <h1 style="color: darkgreen;">Hola soy el componente padre</h1> <br>
         <!-- Se manda a traer el componente el componente "Hijo_1" y se le asigna una edad-->
          <Hijo_1 class="mt-4" nombre="Nombre: Coral Rosas Cruz" edad=" Edad: 25 años" carrera="Carrera: Ingeniería en Sistemas Computacionales" direccion="Dirección: Hipodromo #2" telefono="Teléfono: 953 127 3487"/>
-        <!-- Se manda a traer el componente el componente "Hijo_2" y se le asigna una edad-->
+        <!-- Se manda a traer el componente el componente "Hijo_2" y se le asigna una edad-->   <br>
          <Hijo_2 class="mt-4" nombre="Nombre: Karina Rojas Colores" edad="Edad: 23 años" carrera="Carrera: Licenciatura en Administración" direccion="Dirección: Hipodromo #8" telefono="Teléfono: 221 097 9361"/>
     </div>
    </template>
